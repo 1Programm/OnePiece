04dee206d076432608a9575db4044d21be1813f9
Programm0 <1Pro2gra3mm45@web.de>
2022-10-20 16:30:43 +0200

    Testing stuff

--- Game/src/main/java/com/programm/onepiece/game/Game.java
+++ Game/src/main/java/com/programm/onepiece/game/Game.java
@@ -3,8 +3,11 @@ package com.programm.onepiece.game;
 import com.programm.onepiece.engine.OnePieceEngine;
 
 import java.awt.*;
+import java.awt.event.MouseEvent;
 
 public class Game extends OnePieceEngine {
+
+    private float testX = 100, testY = 100;
 
     public Game(String title, int width, int height, int fps, int logLevel) {
         super(title, width, height, fps, logLevel);
@@ -17,6 +20,12 @@ public class Game extends OnePieceEngine {
 
     protected void onMousePressed(int button){
         logger.info("Mouse pressed at: (" + mouse.x() + ", " + mouse.y() + ")");
+
+        //Left Click
+        if(button == MouseEvent.BUTTON1) {
+            testX = mouse.x();
+            testY = mouse.y();
+        }
     }
 
     @Override
@@ -26,7 +35,12 @@ public class Game extends OnePieceEngine {
 
     @Override
     protected void render(Graphics g) {
+        //Fill Screen White
+        g.setColor(Color.WHITE);
+        g.fillRect(0, 0, window.width(), window.height());
+
+        //Draw Test Rectangle
         g.setColor(Color.RED);
-        g.drawRect(100, 100, 100, 100);
+        g.drawRect((int)testX, (int)testY, 100, 100);
     }
 }
