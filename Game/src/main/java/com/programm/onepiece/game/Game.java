f4bc9415f70e6a1039c0d0c23ea565d6892af23d
Omar <omar.ali001@stud.fh-dortmund.de>
2022-10-24 18:25:22 +0200

    - Game works now with KeyEvent

--- Game/src/main/java/com/programm/onepiece/game/Game.java
+++ Game/src/main/java/com/programm/onepiece/game/Game.java
@@ -3,6 +3,7 @@ package com.programm.onepiece.game;
 import com.programm.onepiece.engine.OnePieceEngine;
 
 import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 
 public class Game extends OnePieceEngine {
@@ -16,6 +17,7 @@ public class Game extends OnePieceEngine {
     @Override
     protected void init() {
         mouse.onMousePressed(this::onMousePressed);
+        keyboard.onKeyPressed(this::onKeyboardPressed);
     }
 
     protected void onMousePressed(int button){
@@ -26,6 +28,19 @@ public class Game extends OnePieceEngine {
             testX = mouse.x();
             testY = mouse.y();
         }
+    }
+
+    protected void onKeyboardPressed(int key) {
+        logger.info(("Key pressed : (" + key + ")"));
+
+        if(key == KeyEvent.VK_W)
+            testY -= 20;
+        if(key == KeyEvent.VK_S)
+            testY += 20;
+        if(key == KeyEvent.VK_A)
+            testX -= 20;
+        if(key == KeyEvent.VK_D)
+            testX += 20;
     }
 
     @Override
@@ -40,7 +55,7 @@ public class Game extends OnePieceEngine {
         g.fillRect(0, 0, window.width(), window.height());
 
         //Draw Test Rectangle
-        g.setColor(Color.RED);
+        g.setColor(Color.BLUE);
         g.drawRect((int)testX, (int)testY, 100, 100);
     }
 }
